ce91331310fb2494c19dcd2411ccf70770912927
aaronhall121901 <113931541+aaronhall121901@users.noreply.github.com>
2024-04-06 17:32:25 -0500

    .

--- answer_spawn.gd
+++ answer_spawn.gd
@@ -4,21 +4,23 @@ extends Node2D
 var questionSet
 
 
-var current_question = 0
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var answer_instance = load("res://path/to/Answers.tscn").instance()
-	add_child(answer_instance)
-
+	#var answer_instance = load("res://path/to/Answers.tscn").instance()
+	#add_child(answer_instance)
+	updateLvl1Questions()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	#Selectes question/answer set from Globalvars based on what level is selected.
 	if GlobalVars.levelSelected == 1:
+		print("rahhh")
 		questionSet = GlobalVars.level1
 	elif GlobalVars.levelSelected == 2:
 		questionSet = GlobalVars.level2
+	updateLvl1Questions()
 	## ADD LEVEL 3 HERE.
+
+func updateLvl1Questions():
+	print(questionSet["question"])
