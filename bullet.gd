ab2e30a11148078ce22fdc5963314f4785df4d01
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-04-11 15:18:45 -0500

    Bullets are incorporated

--- bullet.gd
+++ bullet.gd
@@ -5,9 +5,19 @@ var direction = Vector2.UP
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	position += direction * speed * delta
+	
 	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	GlobalVars.bulletShots += 1
+	queue_free()
+	pass # Replace with function body.
