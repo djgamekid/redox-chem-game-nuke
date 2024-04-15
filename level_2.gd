03d971c95b986bcf847883c03493d497f8e00d9b
Deepshikha Bakshi <99354163+20Deepshikha@users.noreply.github.com>
2024-04-15 13:19:39 -0500

    Created Buttons for level 2

--- level_2.gd
+++ level_2.gd
@@ -9,3 +9,30 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _on_pause_button_pressed():
+	get_tree().paused = true
+	$PauseButton.hide()
+	$HelpButton.show()
+	$ResumeButton.show()
+
+func _on_resume_button_pressed():
+	get_tree().paused = false
+	$Message.hide()
+	$ResumeButton.hide()
+	$HelpButton.hide()
+	$PauseButton.show()
+
+
+func _on_help_button_pressed():
+	$ResumeButton.hide()
+	$HelpButton.hide()
+	$HelpMessage.show()
+	$HelpOkButton.show()
+
+
+func _on_help_ok_button_pressed():
+	$HelpButton.show()
+	$ResumeButton.show()
+	$HelpMessage.hide()
+	$HelpOkButton.hide()
