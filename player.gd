4b3c04e6b922eff30a52b5c4bf94aa91debc73f0
aaronhall121901 <113931541+aaronhall121901@users.noreply.github.com>
2024-04-16 19:30:01 -0500

    Implimented player shooting

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+const bulletPath = preload('res://bullet.tscn')
 
 
 func _physics_process(delta):
@@ -15,5 +15,11 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if Input.is_action_just_pressed('ui_accept'):
+		shoot()
 	move_and_slide()
+
+func shoot():
+	var bullet = bulletPath.instantiate()
+	get_parent().add_child(bullet)
+	bullet.position = $Marker2D.global_position
